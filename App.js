dc32e83dca62375db48be1c0c9311670041aaf35
MaxMandia <mmandia511@gmail.com>
2023-06-19 21:58:49 -0400

    feat: crafted home ui

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState, useRef, useEffect } from "react";
+import { TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.header}>
+        <Text>🧌 Troll Leader 🧌</Text>
+        <Text>Name</Text>
+      </View>
+      <View style={styles.trollRowContainer}>
+        <View style={styles.trollRow}>
+          <View style={styles.nameRow}>
+            <Text>💩 Ryan:</Text>
+            <Text>20</Text>
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.plus}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.trollRow}>
+          <View style={styles.nameRow}>
+            <Text>😇 Max:</Text>
+            <Text>20</Text>
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.plus}>+</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    position: "absolute",
+    top: "10%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  trollRowContainer: {
+    width: "100%",
+  },
+  trollRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10,
+    marginVertical: 2,
+    paddingHorizontal: 20,
+  },
+  nameRow: {
+    flexDirection: "row",
+  },
+  plus: {
+    fontSize: 30,
   },
 });
